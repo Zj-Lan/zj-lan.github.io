defb9b83549cf3d79da3a36f7013d1d55ac54577
Zj-Lan <zlan@mun.ca>
2021-01-02 14:58:40 +0000

    Deploying to gh-pages from  @ b40287831bebf0b5bf35fba2357fd29ad5f491a4 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b50a111b3814d6c4eb67f4203abb7841.js"
+  "/portfolio/precache-manifest.3ba4f5b4daecb8687c7c4400459d02b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
